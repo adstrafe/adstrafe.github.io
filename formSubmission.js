4660da080746aa026c99c1a689322224fc86a433
ADStrafe <vojtech.lahuta@gmail.com>
2025-03-16 01:53:56 +0100

    bruh idk

--- formSubmission.js
+++ formSubmission.js
@@ -1,19 +1,21 @@
-document.querySelector('.form').addEventListener('submit', function (e) {
+document.querySelector('.form').addEventListener('submit', async function (e) {
 	e.preventDefault();
 
-	var formData = new FormData(this);
-	fetch('https://formspree.io/f/movevelz', {
+	const formData = new FormData(this);
+	const response = await fetch('https://formspree.io/f/movevelz', {
 		method: 'POST',
 		body: formData
-	})
-		.then(response => response.json())
-		.then(data => {
-			document.getElementById('success-message').style.display = 'block';
-			document.getElementById('error-message').style.display = 'none';
-			this.reset();
-		})
-		.catch(error => {
-			document.getElementById('error-message').style.display = 'block';
-			document.getElementById('success-message').style.display = 'none';
-		});
+	});
+
+	if (response.status !== 200) {
+		const error = await response.json();
+		console.error(error);
+		document.getElementById('error-message').style.display = 'block';
+		document.getElementById('success-message').style.display = 'none';
+	}
+
+	console.log('????');
+	document.getElementById('success-message').style.display = 'block';
+	document.getElementById('error-message').style.display = 'none';
+	this.reset();
 });
